48a70a6304a7ea4a6fe40cf5e7b303ac1082b085
n43-3m <naeemur006@gmail.com>
2025-02-16 02:20:37 +0600

    fix padding and add divider line in full post page

--- src/FullPostPage.jsx
+++ src/FullPostPage.jsx
@@ -3,8 +3,11 @@ import ContentCard from "./ContentCard";
 
 export default function FullPostPage() {
     return (
-        <div className="flex flex-col justify-start items-center px-1">
+        <div className="flex flex-col justify-start items-center px-1 py-3 md:py-4 lg:py-5">
             <ContentCard/>
+            <div className="w-full h-[2px] bg-white/50 my-8 rounded-full">
+                
+            </div>
             <ContentCard/>
             <ContentCard/>
             
